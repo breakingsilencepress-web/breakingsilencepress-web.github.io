c8295e4dfbe4534dda5d6dbb48838798b5c23932
breakingsilencepress-web <breakingsilencepress@gmail.com>
2026-04-25 14:22:32 +0500

    Add files via upload

---
+++ frontend/js/evidence.js
@@ -0,0 +1,135 @@
+const documentNames = document.querySelectorAll('.evidence-cards-heading');
+const searchBar = document.getElementById('searchBarInput');
+const searchIcon = document.getElementById('searchIcon');
+const noDocument = document.querySelector('.no-document');
+const evidenceCards = document.querySelector('.evidence-cards');
+const readMorePopup = document.querySelector('.readmore-popup');
+let documentsVisible = [];
+evidenceCardData = [];
+
+getEvidences();
+
+searchIcon.addEventListener('click', () => {
+    documentsVisible = [];
+    documentNames.forEach(doc => {
+        if(!doc.textContent.toLowerCase().startsWith(searchBar.value.toLowerCase())){
+            doc.closest('.card').classList.add('hide');
+        } else {
+            doc.closest('.card').classList.remove('hide');
+        }
+        if(!doc.closest('.card').classList.contains('hide')){
+            documentsVisible.push(doc);
+        }
+
+    });
+    if(documentsVisible.length == 0){
+        noDocument.style.display = "flex";
+    } else {
+        noDocument.style.display = "none";
+    }
+});
+
+async function getEvidences(){
+    evidenceCardData.splice(0, evidenceCardData.length);
+    try {
+        const response = await fetch("http://localhost:3000/evidences-public");
+        const data = await response.json();
+        if (response.ok){
+            data.evidences.forEach(evidence => evidenceCardData.push(evidence));
+            renderEvidences();
+        }
+    } catch (err) {
+        // silent fail
+    }
+}
+
+function renderEvidences(){
+    grid.innerHTML = "";
+    evidenceCards.innerHTML = "";
+    evidenceCardData.forEach(evidence => {
+        const date = new Date(evidence.date).toLocaleDateString('en-US', {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric'
+        });
+        const card = document.createElement('div');
+        card.className = "card";
+        card.innerHTML = `
+        <div class="tags">
+            <p class="qr-code">${evidence.qrCode}</p>
+            <p class="chapter">${evidence.chapter}</p>
+        </div>
+        <h3 class="evidence-cards-heading">${evidence.heading}</h3>
+        <p class="date"><i class="fa-solid fa-calendar"></i>${date}</p>
+        <p class="card-info">${evidence.description}</p>
+        <div class="card-cta">
+            <button class="download"><i class="fa-solid fa-download"></i> Download</button>
+            <button class="read"><i class="fa-brands fa-readme"></i> Read Online</button>
+        </div>
+        <button class="read-more" id="readMoreBtn">Read More</button>`;
+        evidenceCards.appendChild(card);
+        const readMoreBtn = card.querySelector('.read-more');
+        readMoreBtn.addEventListener('click', () => {
+            readMorePopup.classList.add('show');
+            const readmoreWrapper = document.createElement('div');
+            readmoreWrapper.className = "readmore-wrapper";
+            readmoreWrapper.innerHTML = `<div class="header">
+                <div class="header-start">
+                    <p class="qr-code">${evidence.qrCode}</p>
+                    <h3 class="heading">${evidence.heading}</h3>
+                </div>
+                <button class="close-btn" id="closeReadMorePopup">X</button>
+            </div>
+            <div class="content">
+                <div class="source-box readmore-box">
+                    <p class="heading">SOURCE</p>
+                    <p class="info">${evidence.source}</p>
+                </div>
+                <div class="filetype-box readmore-box">
+                    <p class="heading">FILE TYPE</p>
+                    <p class="info">${evidence.fileType}</p>
+                </div>
+                <div class="download-date-box readmore-box">
+                    <p class="heading">DOWNLOAD DATE</p>
+                    <p class="info">${evidence.downloadDate}</p>
+                </div>
+                <div class="download-date-box readmore-box">
+                    <p class="heading">COPYRIGHT</p>
+                    <p class="info">${evidence.copyRightStatus}</p>
+                </div>
+                <div class="orignal-url-box readmore-box">
+                    <p class="heading">ORIGINAL URL</p>
+                    <p class="info url">${evidence.originalUrl}</p>
+                </div>
+                <div class="description-box readmore-box">
+                    <p class="heading">DESCRIPTION</p>
+                    <p class="info url">${evidence.moreDescription}</p>
+                </div>
+                <div class="relevance-box readmore-box">
+                    <p class="heading">RELEVANCE</p>
+                    <p class="info">${evidence.relevance}</p>
+                </div>
+            </div>
+            <div class="readmore-cta">
+                <button class="readmore-close">Close</button>
+                <button class="readmore-download">Download</button>
+            </div>`
+            const closeReadMorePopup = readmoreWrapper.querySelector('.close-btn');
+            const closeReadMorePopupButton = readmoreWrapper.querySelector('.readmore-close');
+
+            if(closeReadMorePopupButton){
+                closeReadMorePopupButton.addEventListener('click', () => {
+                    readMorePopup.classList.remove("show")
+                });
+            }
+
+            if(closeReadMorePopup){
+                closeReadMorePopup.addEventListener('click', () => {
+                    readMorePopup.classList.remove("show")
+                });
+            }
+            readMorePopup.innerHTML = "";
+            readMorePopup.appendChild(readmoreWrapper);
+        });
+    });
+};
